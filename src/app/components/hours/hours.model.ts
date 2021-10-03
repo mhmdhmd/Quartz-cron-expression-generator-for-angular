a27bf8b48e353ef7afeed4efb74585977b634f53
Hamid Mohammadi <hmd.tradeinfo@gmail.com>
2021-10-03 19:21:13 +0330

    fix models

--- src/app/components/hours/hours.model.ts
+++ src/app/components/hours/hours.model.ts
@@ -1,21 +1,7 @@
-import { Custom as sharedCustom, Type, Repeat as sharedRepeat, Between as sharedBetween } from "../shared.model";
+import { Custom, Type} from "../shared.model";
 
 
 export class Hours{
     type: Type = Type.Every;
     custom: Custom = new Custom();
-}
-
-export class Repeat extends sharedRepeat{
-    repeatStartAt: number = 0;
-}
-
-export class Between extends sharedBetween{
-    betweenFrom: number = 0;
-    betweenTo: number = 0;
-}
-
-export class Custom extends sharedCustom{
-    repeat: Repeat = new Repeat();
-    between: Between = new Between();
 }
--- src/app/components/shared.model.ts
+++ src/app/components/shared.model.ts
@@ -1,11 +1,14 @@
 
 export class Custom{
     specific: Specific = new Specific();
+    repeat: Repeat = new Repeat();
+    between: Between = new Between();
 }
 
 export class Repeat{
     isRepeat: boolean = false;
     repeatInterval: number = 1;
+    repeatStartAt: number = 0;
 }
 
 export class Specific{
@@ -15,6 +18,8 @@ export class Specific{
 
 export class Between{
     isBetween: boolean = false;
+    betweenFrom: number = 0;
+    betweenTo: number = 0;
 }
 
 export enum Type{
